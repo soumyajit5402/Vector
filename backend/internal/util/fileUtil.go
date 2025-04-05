7964a61bcf57c098373653afd9564016eef83916
Soumyajit Das <128028081+das-soumyajit@users.noreply.github.com>
2025-04-05 22:33:29 +0530

    Add comments for utils

--- backend/internal/util/fileUtil.go
+++ backend/internal/util/fileUtil.go
@@ -5,14 +5,17 @@ import (
     "path/filepath"
 )
 
+// EnsureDirectoryExists ensures that a directory exists
 func EnsureDirectoryExists(dir string) error {
     return os.MkdirAll(dir, os.ModePerm)
 }
 
+// GetFileInfo gets the file info for a given path
 func GetFileInfo(path string) (os.FileInfo, error) {
     return os.Stat(path)
 }
 
+// JoinPath joins a list of elements into a path
 func JoinPath(elem ...string) string {
     return filepath.Join(elem...)
 } 
